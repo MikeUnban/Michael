affbb23a98c1ef83217b8154f9f2d28cf94f5342
MikeUnban <103874175+MikeUnban@users.noreply.github.com>
2022-04-19 15:40:25 +0700

    Update app.js

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ const HOST = '0.0.0.0'
 
 const app = express()
 app.get('/', (req, res) => {
-    res.send('Michael! 52019021 Teknik Informatika')
+    res.send('Michael Christian Sugianto</br/> 52019021</br/> Teknik Informatika')
 })
 
 app.listen(PORT, HOST)
